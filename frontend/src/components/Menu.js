bdcfbb8f5250fc1b22ed1cdc58f78d75c6028b6d
chamorcev <chamorcev@gmail.com>
2021-12-13 02:34:35 +0300

    Lesson 2

---
+++ frontend/src/components/Menu.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const MenuItem = ({menu}) => {
+   return (
+       <tr>
+           <td>
+               {menu.main}
+           </td>
+           <td>
+               {menu.api}
+           </td>
+       </tr>
+   )
+}
+
+const MenuList = ({menus}) => {
+    return (
+        <table>
+            <th>
+                main
+            </th>
+            <th>
+                api
+            </th>
+            {menus.map((menu) => <MenuItem menu={menu} />)}
+        </table>
+    )
+ }
+
+export default MenuList
