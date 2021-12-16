c219b41a2ae5a345ec047109e51c7a6a327b2e4e
chamorcev <chamorcev@gmail.com>
2021-12-16 11:03:34 +0300

    Lesson 3

---
+++ todo/projects/views.py
@@ -0,0 +1,16 @@
+from rest_framework.viewsets import ModelViewSet
+from .serializers import ProjectModelSerializer, TodoModelSerializer
+from .models import Project, Todo
+
+
+class ProjectModelViewSet(ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectModelSerializer
+
+
+class TodoModelViewSet(ModelViewSet):
+    queryset = Todo.objects.all()
+    serializer_class = TodoModelSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(is_active=True)
--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'todo',
     'users',
+    'projects',
 ]
 
 MIDDLEWARE = [
